c9a1e7a9457fe0b1fe72ce5b2331c2ddc5fb48fa
Gustavo Maia <gustavommilitao06@gmail.com>
2026-05-01 22:00:19 -0300

    :truck: chore: feature-first

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -20,21 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: const Center(
-        child: Text('Tela inicial do app'),
-      ),
-    );
-  }
-}
