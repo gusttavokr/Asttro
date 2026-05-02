74020cf80d4935474675fbb7b453e41bfaddbf57
Gustavo Maia <gustavommilitao06@gmail.com>
2026-05-01 22:04:19 -0300

    :white_check_mark: test: feature_first

--- frontend/lib/features/Home/home.dart
+++ frontend/lib/features/Home/home.dart
@@ -7,9 +7,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
       body: const Center(
         child: Text('Tela inicial do app'),
       ),
