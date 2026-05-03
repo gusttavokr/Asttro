3df8843c0b32b4993f866e0982df7f5019c0bfa3
Gustavo Maia <gustavommilitao06@gmail.com>
2026-05-03 16:15:26 -0300

    :sparkles: feat: background

---
+++ frontend/lib/core/widgets/background.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppBackground extends StatelessWidget {
+  const AppBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF0D0D1B),
+    );
+  }
+}
