ef7e45903d1537f9cf6bc615e36c44bb36ddb9c2
Gustavo Maia <gustavommilitao06@gmail.com>
2026-05-03 21:31:54 -0300

    :lipstick: feat: statusBar light

--- frontend/lib/features/Home/home.dart
+++ frontend/lib/features/Home/home.dart
@@ -1,29 +1,36 @@
 import 'dart:async';
 import 'package:asttro/core/widgets/background.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          const AppBackground(),
-          const SafeArea(
-            child: Padding(
-              padding: EdgeInsets.all(24),
-              child: Text(
-                'Você tem Tarefas Pendentes',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+          statusBarColor: Colors.white,
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.dark),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            const AppBackground(),
+            const SafeArea(
+              child: Padding(
+                padding: EdgeInsets.all(24),
+                child: Text(
+                  'Você tem Tarefas Pendentes',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
